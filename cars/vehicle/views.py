a3acf6c09efd7e2b4060a69ab8ac48d3ef25a69e
josue santos <josue.santos.dev@gmail.com>
2023-05-10 02:59:38 -0300

    [view] (vehicle) view

--- cars/vehicle/views.py
+++ cars/vehicle/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ninja import Router
+
+class VehicleView():
+    router = Router()
+
+    router.post("/")
+    def create(self):
+        pass
+    
+    router.get("/")
+    def view(self):
+        return {"result": "carsSerialized"}
+    
+    router.put("/")
+    def update(self):
+        pass
+    
+    router.delete("/")
+    def delete(self):
+        pass 
+    
