4fc806077b5a39f6fafd0fa37b28dc723d3cb935
josue santos <josue.santos.dev@gmail.com>
2023-05-10 02:39:45 -0300

    [model] vehicle

--- cars/vehicle/models.py
+++ cars/vehicle/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Model):
+    title = models.CharField('Name',
+                             max_length=150)
+    brand = models.CharField('Brand',
+                             max_length=50)
+    model = models.CharField('Model',
+                             max_length=50)
+    photo = models.CharField('Photo',
+                             max_length=1024)
+    description = models.CharField('Description',
+                             max_length=526)
+    price = models.IntegerField('Price')
+
