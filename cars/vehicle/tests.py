384b32200621c3546c26b16b8b5c27fe886a2e76
josue santos <josue.santos.dev@gmail.com>
2023-05-10 02:40:11 -0300

    [test] (vehicle) partial test

--- cars/vehicle/tests.py
+++ cars/vehicle/tests.py
@@ -1,16 +1,21 @@
 from django.test import TestCase
 
+from .models import Vehicle
+
 class VehicleTest(TestCase):
     
 
     def test_create_vehicle():
-        pass
+        Vehicle.objects.create(
+            title       = 'novo civic',
+            brand       = 'honda',
+            model       = 'civic',
+            description = 'novo civic 2023, maximo conforto'
+        )
     
-    def test_create_invalid_vehicle():
-        pass
-    
-    def test_get_vehicle():
-        pass
+    def test_get_vehicle(self):
+        vehicle = Vehicle.objects.get(title = 'novo civic')
+        self.assertEquals(vehicle.brand, 'honda')
     
     def test_list_vehicle():
         pass
@@ -21,8 +26,3 @@ class VehicleTest(TestCase):
     def test_delete_vehicle():
         pass
     
-    def test_update_invalid_vehicle():
-        pass
-    
-    def test_delete_invalid_vehicle():
-        pass
