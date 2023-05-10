1c541cf36f925f27a74ff7c87fc29706ebadc152
josue santos <josue.santos.dev@gmail.com>
2023-05-10 02:15:17 -0300

    tests base

--- cars/vehicle/tests.py
+++ cars/vehicle/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+class VehicleTest(TestCase):
+    
+
+    def test_create_vehicle():
+        pass
+    
+    def test_create_invalid_vehicle():
+        pass
+    
+    def test_get_vehicle():
+        pass
+    
+    def test_list_vehicle():
+        pass
+    
+    def test_update_vehicle():
+        pass
+    
+    def test_delete_vehicle():
+        pass
+    
+    def test_update_invalid_vehicle():
+        pass
+    
+    def test_delete_invalid_vehicle():
+        pass
